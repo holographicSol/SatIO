d9dc73c9ac48f30245e15401d2b0760a9718ad40
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-11-06 20:00:12 +0000

    Update satIO_001.cpp

--- 241029-193618-esp32dev/src/satIO_001.cpp
+++ 241029-193618-esp32dev/src/satIO_001.cpp
@@ -306,12 +306,11 @@ validationStruct validData;
 
 // ----------------------------------------------------------------------------------------------------------------------------
 //                                                                                                               ANALOGUE WRITE
-// Arduino like analogWrite
+
 // value has to be between 0 and valueMax
 void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
   // calculate duty, 4095 from 2 ^ 12 - 1
   uint32_t duty = (4095 / valueMax) * min(value, valueMax);
-
   // write duty to LEDC
   ledcWrite(channel, duty);
 }
