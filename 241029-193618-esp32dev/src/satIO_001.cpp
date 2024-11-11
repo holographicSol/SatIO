56263a833e4db94cfd2f230f11ff07f273bb905e
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-11-11 14:07:59 +0000

    Add files via upload

--- 241029-193618-esp32dev/src/satIO_001.cpp
+++ 241029-193618-esp32dev/src/satIO_001.cpp
@@ -7863,7 +7863,7 @@ void setup() {
   // ESP32 can map hardware serial to alternative pins. Map Serial1 for GPS module to the following, we will need this on CYD
   Serial1.setPins(rxd_from_gps, txd_to_atmega, ctsPin, rtsPin);
   Serial1.begin(115200);
-  Serial1.setTimeout(500);
+  Serial1.setTimeout(300);
   Serial1.flush();
 
   // ----------------------------------------------------------------------------------------------------------------------------
