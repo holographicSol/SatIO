7fa161d68f8fafb3beeb781a4e57551465dc8941
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-11-16 14:35:24 +0000

    Add files via upload

--- 241029-193618-esp32dev/src/satIO_001.cpp
+++ 241029-193618-esp32dev/src/satIO_001.cpp
@@ -6420,7 +6420,7 @@ bool DisplayPage0() {
 
     char ground_heading_name[10];
 
-    for (int i = 0; i<16; i++) {
+    for (int i = 0; i<15; i++) {
       if (atof(gnrmcData.ground_heading) >= ground_heading_range[0][i][0] && atof(gnrmcData.ground_heading) <= ground_heading_range[0][i][1]) {
         uiData.mapped_ground_heading = map(atof(gnrmcData.ground_heading), ground_heading_range[0][i][0], ground_heading_range[0][i][1], 50+uiData.yaw_x, 100+uiData.yaw_x);
         memset(ground_heading_name, 0, sizeof(ground_heading_name));
