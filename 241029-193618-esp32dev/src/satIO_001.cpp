f2a9dc8616c3332a63de84ab1bb318de1725332d
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-11-17 14:17:00 +0000

    Add files via upload

--- 241029-193618-esp32dev/src/satIO_001.cpp
+++ 241029-193618-esp32dev/src/satIO_001.cpp
@@ -6507,8 +6507,8 @@ bool DisplayPage0() {
     // Serial.println("[mapped ground name] " + String(name_ground_heading));
     // Serial.println("[mapped ground_heading pixel] " + String(uiData.mapped_ground_heading));
     // Serial.println("[i_mapped_ground_heading] " + String(i_mapped_ground_heading));
-    hud.fillRect(uiData.yaw_x-50, uiData.pitch_y-17, 50, 16, TFT_BLACK); // wipe any left ground heading overrun 
-    hud.fillRect(uiData.yaw_x+118, uiData.pitch_y-17, 52, 16, TFT_BLACK); // wipe any right ground heading overrun 
+    hud.fillRect(uiData.yaw_x-50, uiData.pitch_y-17, 40, 16, TFT_BLACK); // wipe any left ground heading overrun 
+    hud.fillRect(uiData.yaw_x+110, uiData.pitch_y-17, 40, 16, TFT_BLACK); // wipe any right ground heading overrun 
     hud.drawRect(uiData.yaw_x-20, uiData.pitch_y-18, 140, 16, TFTOBJ_COL0); // display ground heading rect
     
     // Ground Heading Degrees:
