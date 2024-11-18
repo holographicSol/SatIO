83f365414c2f67e7266728d8beca8cb9facbf49b
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-11-18 05:14:12 +0000

    Add files via upload

--- 241029-193618-esp32dev/src/satIO_001.cpp
+++ 241029-193618-esp32dev/src/satIO_001.cpp
@@ -6451,7 +6451,7 @@ bool DisplayPage0() {
     hud.drawString(String(gpattData.mileage)+String(""), (uiData.yaw_x+75)+8, rdata_y+18*8+8);
 
     // Ground Heading Name and Degrees:
-    memset(gnrmcData.ground_heading, 0, sizeof(gnrmcData.ground_heading)); strcpy(gnrmcData.ground_heading, "358.00");  // uncomment to test (this will be getting overwritten periodically if testing uncommented)
+    // memset(gnrmcData.ground_heading, 0, sizeof(gnrmcData.ground_heading)); strcpy(gnrmcData.ground_heading, "358.00");  // uncomment to test (this will be getting overwritten periodically if testing uncommented)
     uiData.mapped_ground_heading = atof(gnrmcData.ground_heading);
     int i_mapped_ground_heading = 0;
     Serial.println("[gnrmc ground heading] " + String(uiData.mapped_ground_heading));
@@ -6476,8 +6476,6 @@ bool DisplayPage0() {
     hud.setTextDatum(MC_DATUM);
 
     hud.drawRect(uiData.yaw_x-60, uiData.pitch_y-18, 220, 16, TFTOBJ_COL0); // display ground heading rect
-
-    
 
     if (!i_mapped_ground_heading == 0 && !i_mapped_ground_heading == 15 && !i_mapped_ground_heading == 1) {
         Serial.println("[i check 1] ");
