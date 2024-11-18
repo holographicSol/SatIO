54a321f8da0bd617aafdc380d73d15c1575b18d9
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-11-18 14:15:13 +0000

    Add files via upload

--- 241029-193618-esp32dev/src/satIO_001.cpp
+++ 241029-193618-esp32dev/src/satIO_001.cpp
@@ -6478,7 +6478,7 @@ bool DisplayPage0() {
     hud.drawString(String(gnggaData.longitude_hemisphere), uiData.yaw_x+51+69+10, 232);
 
     // Ground Heading Name and Degrees:
-    memset(gnrmcData.ground_heading, 0, sizeof(gnrmcData.ground_heading)); strcpy(gnrmcData.ground_heading, "22.5");  // uncomment to test (this will be getting overwritten periodically if testing uncommented)
+    // memset(gnrmcData.ground_heading, 0, sizeof(gnrmcData.ground_heading)); strcpy(gnrmcData.ground_heading, "22.5");  // uncomment to test (this will be getting overwritten periodically if testing uncommented)
     uiData.mapped_ground_heading = atof(gnrmcData.ground_heading);
     int i_mapped_ground_heading = 0;
     for (int i = 0; i<16; i++) {
