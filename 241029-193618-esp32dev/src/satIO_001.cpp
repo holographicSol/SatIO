8cb8b7ed185f4e3e03741faf483f8bbe6dc1525a
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-12-08 16:36:58 +0000

    Add files via upload
    added boot splash image

--- 241029-193618-esp32dev/src/satIO_001.cpp
+++ 241029-193618-esp32dev/src/satIO_001.cpp
@@ -93,7 +93,7 @@
 #include <XPT2046_Bitbang.h>  // https://github.com/ddxfish/XPT2046_Bitbang_Arduino_Library/
 #include <SiderealPlanets.h>  // https://github.com/DavidArmstrong/SiderealPlanets
 #include <SiderealObjects.h>  // https://github.com/DavidArmstrong/SiderealObjects
-#include <TFT_eSPI_Scroll.h>
+#include <JPEGDecoder.h>
 
 int MAX_GPS_RETIES = 0;
 
@@ -121,8 +121,6 @@ TFT_eSPI tft = TFT_eSPI();
 TFT_eSPI_Button key[6];
 TFT_eSprite hud = TFT_eSprite(&tft);        // general sprite
 TFT_eSprite uap = TFT_eSprite(&tft);        // INS roll sprite
-TFT_eSPI_Scroll scroll_text_0;
-String scroll_text_data_0;
 
 #define LCD_BACK_LIGHT_PIN 21    // backlight pin
 #define LEDC_CHANNEL_0     0     // backlight: use first channel of 16 channels (started from zero)
@@ -232,7 +230,7 @@ sysDebugStruct sysDebugData;
 //                                                                                                                     DATA: MENU
 
 struct menuStruct {
-  char input[2024];                // char array input by user
+  char input[100];                // char array input by user
   int numpad_key = NULL;           // allows numpad to differentiate between values
   int page = 0;                    // currently displayed page
   int backpage = 0;                // page we would like to return to after current page
@@ -288,7 +286,7 @@ struct SerialLinkStruct {
   bool data = false;
   char BUFFER[2000];           // read incoming bytes into this buffer
   char BUFFER1[2000];               // buffer refined using ETX
-  char TMP[2000];               // buffer refined using ETX
+  // char TMP[2000];               // buffer refined using ETX
   unsigned long nbytes;
   unsigned long TOKEN_i;
   unsigned long T0_RXD_1 = 0;   // hard throttle current time
@@ -8309,6 +8307,132 @@ void SatIOPortController() {
   }
 }
 
+
+// ------------------------------------------------------------------------------------------------------------------------------
+//                                                                                                        DISPLAY JPG FROM SDCARD
+
+void jpegInfo() {
+  Serial.println("JPEG image info");
+  Serial.println("===============");
+  Serial.print("Width      :");
+  Serial.println(JpegDec.width);
+  Serial.print("Height     :");
+  Serial.println(JpegDec.height);
+  Serial.print("Components :");
+  Serial.println(JpegDec.comps);
+  Serial.print("MCU / row  :");
+  Serial.println(JpegDec.MCUSPerRow);
+  Serial.print("MCU / col  :");
+  Serial.println(JpegDec.MCUSPerCol);
+  Serial.print("Scan type  :");
+  Serial.println(JpegDec.scanType);
+  Serial.print("MCU width  :");
+  Serial.println(JpegDec.MCUWidth);
+  Serial.print("MCU height :");
+  Serial.println(JpegDec.MCUHeight);
+  Serial.println("===============");
+  Serial.println("");
+}
+
+void showTime(uint32_t msTime) {
+  Serial.print(F(" JPEG drawn in "));
+  Serial.print(msTime);
+  Serial.println(F(" ms "));
+}
+
+
+// Draw a JPEG on the TFT, images will be cropped on the right/bottom sides if they do not fit
+// This function assumes xpos,ypos is a valid screen coordinate. For convenience images that do not
+// fit totally on the screen are cropped to the nearest MCU size and may leave right/bottom borders.
+void jpegRender(int xpos, int ypos) {
+  jpegInfo(); // Print information from the JPEG file (could comment this line out)
+  uint16_t *pImg;
+  uint16_t mcu_w = JpegDec.MCUWidth;
+  uint16_t mcu_h = JpegDec.MCUHeight;
+  uint32_t max_x = JpegDec.width;
+  uint32_t max_y = JpegDec.height;
+  bool swapBytes = tft.getSwapBytes();
+  tft.setSwapBytes(true);
+  // Jpeg images are draw as a set of image block (tiles) called Minimum Coding Units (MCUs)
+  // Typically these MCUs are 16x16 pixel blocks
+  // Determine the width and height of the right and bottom edge image blocks
+  uint32_t min_w = jpg_min(mcu_w, max_x % mcu_w);
+  uint32_t min_h = jpg_min(mcu_h, max_y % mcu_h);
+  // save the current image block size
+  uint32_t win_w = mcu_w;
+  uint32_t win_h = mcu_h;
+  // record the current time so we can measure how long it takes to draw an image
+  uint32_t drawTime = millis();
+  // save the coordinate of the right and bottom edges to assist image cropping
+  // to the screen size
+  max_x += xpos;
+  max_y += ypos;
+  // Fetch data from the file, decode and display
+  while (JpegDec.read()) {    // While there is more data in the file
+    pImg = JpegDec.pImage ;   // Decode a MCU (Minimum Coding Unit, typically a 8x8 or 16x16 pixel block)
+    // Calculate coordinates of top left corner of current MCU
+    int mcu_x = JpegDec.MCUx * mcu_w + xpos;
+    int mcu_y = JpegDec.MCUy * mcu_h + ypos;
+    // check if the image block size needs to be changed for the right edge
+    if (mcu_x + mcu_w <= max_x) win_w = mcu_w;
+    else win_w = min_w;
+    // check if the image block size needs to be changed for the bottom edge
+    if (mcu_y + mcu_h <= max_y) win_h = mcu_h;
+    else win_h = min_h;
+    // copy pixels into a contiguous block
+    if (win_w != mcu_w)
+    {
+      uint16_t *cImg;
+      int p = 0;
+      cImg = pImg + win_w;
+      for (int h = 1; h < win_h; h++)
+      {
+        p += mcu_w;
+        for (int w = 0; w < win_w; w++)
+        {
+          *cImg = *(pImg + w + p);
+          cImg++;
+        }
+      }
+    }
+    // calculate how many pixels must be drawn
+    uint32_t mcu_pixels = win_w * win_h;
+    // draw image MCU block only if it will fit on the screen
+    if (( mcu_x + win_w ) <= tft.width() && ( mcu_y + win_h ) <= tft.height())
+      tft.pushImage(mcu_x, mcu_y, win_w, win_h, pImg);
+    else if ( (mcu_y + win_h) >= tft.height())
+      JpegDec.abort(); // Image has run off bottom of screen so abort decoding
+  }
+  tft.setSwapBytes(swapBytes);
+  showTime(millis() - drawTime); // These lines are for sketch testing only
+}
+
+// Draw a JPEG on the TFT pulled from SD Card
+// xpos, ypos is top left corner of plotted image
+void drawSdJpeg(const char *filename, int xpos, int ypos) {
+  // Open the named file (the Jpeg decoder library will close it)
+  File jpegFile = SD.open( filename, FILE_READ);  // or, file handle reference for SD library
+  if ( !jpegFile ) {
+    Serial.print("ERROR: File \""); Serial.print(filename); Serial.println ("\" not found!");
+    return;
+  }
+  Serial.println("===========================");
+  Serial.print("Drawing file: "); Serial.println(filename);
+  Serial.println("===========================");
+  // Use one of the following methods to initialise the decoder:
+  bool decoded = JpegDec.decodeSdFile(jpegFile);  // Pass the SD file handle to the decoder,
+  //bool decoded = JpegDec.decodeSdFile(filename);  // or pass the filename (String or character array)
+  if (decoded) {
+    // print information about the image to the serial port
+    jpegInfo();
+    // render the image onto the screen at given coordinates
+    jpegRender(xpos, ypos);
+  }
+  else {
+    Serial.println("Jpeg file format not supported!");
+  }
+}
+
 // ------------------------------------------------------------------------------------------------------------------------------
 //                                                                                                                          SETUP
 
@@ -8356,6 +8480,32 @@ void setup() {
   tft.setFreeFont(FONT5X7_H);
   ledcAnalogWrite(LEDC_CHANNEL_0, 255);
 
+  // ----------------------------------------------------------------------------------------------------------------------------
+  //                                                                                                                SETUP: SDCARD
+
+  if (SD.begin(SS, sdspi, 80000000)) {
+    Serial.println("[sdcard] initialized.");
+    sdcardCheck();
+    sdcard_mkdirs();
+    // load system configuration file
+    if (!sdcard_load_system_configuration(SD, sdcardData.sysconf, 0)) {sdcard_save_system_configuration(SD, sdcardData.sysconf, 0);}
+    // load matrix file specified by configuration file
+    if (!sdcard_load_matrix(SD, sdcardData.matrix_filepath)) {
+      Serial.println("[sdcard] specified matrix file not found!");
+      // create default matrix file
+      if (strcmp(sdcardData.matrix_filepath, sdcardData.default_matrix_filepath)==0) {
+        Serial.println("[sdcard] default matrix file not found!");
+        if (!sdcard_save_matrix(SD, sdcardData.matrix_filepath)) {Serial.println("[sdcard] failed to write default marix file.");}
+        else if (!sdcard_load_matrix(SD, sdcardData.default_matrix_filepath)) {Serial.println("[sdcard] failed to load matrix file");}
+      }
+    }
+  }
+  else {Serial.println("[sdcard] failed to initialize.");}
+
+  drawSdJpeg("/DATA/test.jpg", (tft.width()/2)-120, 0);     // This draws a jpeg pulled off the SD Card
+  delay(5000);
+  tft.fillScreen(TFT_BLACK);  // clear screen before writing to it
+
   // Create touchscreen task to increase performance (core 0 also found to be best for this task)
   xTaskCreatePinnedToCore(
       TouchScreenInput, /* Function to implement the task */
@@ -8380,28 +8530,6 @@ void setup() {
   //                                                                                                      SETUP: SIDEREAL PLANETS
 
   myAstro.begin();
-
-  // ----------------------------------------------------------------------------------------------------------------------------
-  //                                                                                                                SETUP: SDCARD
-
-  if (SD.begin(SS, sdspi, 80000000)) {
-    Serial.println("[sdcard] initialized.");
-    sdcardCheck();
-    sdcard_mkdirs();
-    // load system configuration file
-    if (!sdcard_load_system_configuration(SD, sdcardData.sysconf, 0)) {sdcard_save_system_configuration(SD, sdcardData.sysconf, 0);}
-    // load matrix file specified by configuration file
-    if (!sdcard_load_matrix(SD, sdcardData.matrix_filepath)) {
-      Serial.println("[sdcard] specified matrix file not found!");
-      // create default matrix file
-      if (strcmp(sdcardData.matrix_filepath, sdcardData.default_matrix_filepath)==0) {
-        Serial.println("[sdcard] default matrix file not found!");
-        if (!sdcard_save_matrix(SD, sdcardData.matrix_filepath)) {Serial.println("[sdcard] failed to write default marix file.");}
-        else if (!sdcard_load_matrix(SD, sdcardData.default_matrix_filepath)) {Serial.println("[sdcard] failed to load matrix file");}
-      }
-    }
-  }
-  else {Serial.println("[sdcard] failed to initialize.");}
 
   menuData.page=0;
 }
@@ -8511,20 +8639,48 @@ void loop() {
   timeData.mainLoopTimeStart = millis();
 
   /* take a snapshot of sensory and calculated data */
+
+  int t0 = millis(); 
   readGPS();
+  Serial.println("[gps] " + String(millis()-t0));
+
+  t0 = millis();
   check_gngga();
+  Serial.println("[gngga] " + String(millis()-t0));
+
+  t0 = millis();
   check_gnrmc();
+  Serial.println("[gnrmc] " + String(millis()-t0));
+
+  t0 = millis();
   check_gpatt();
+  Serial.println("[gpatt] " + String(millis()-t0));
+
+  t0 = millis();
   satIOData();
+  Serial.println("[satio] " + String(millis()-t0));
+
+  t0 = millis();
   trackPlanets();
+  Serial.println("[planet track] " + String(millis()-t0));
+
+  t0 = millis();
   sdcardCheck();
+  Serial.println("[sdcard] " + String(millis()-t0));
 
   /* calculate switches */
+  t0 = millis();
   MatrixSwitchTask();
+  Serial.println("[matrix] " + String(millis()-t0));
+
+  t0 = millis();
   MatrixStatsCounter();
+  Serial.println("[matrix counter] " + String(millis()-t0));
 
   /* instruct the portcontroller */
+  t0 = millis();
   SatIOPortController();
+  Serial.println("[port controller] " + String(millis()-t0));
 
   if (interrupt_second_counter > 0) {
     portENTER_CRITICAL(&second_timer_mux);
