f825463d59885ab8fdd9d3e2d2165f91591d6f04
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-02-20 07:45:03 +0000

    Add files via upload

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -186,7 +186,7 @@ Uncomment whatever type you're using!
 #define DHTTYPE DHT11 // DHT 11
 // #define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
 // #define DHTTYPE DHT21 // DHT 21 (AM2301)
-DHT dht(CD74HC4067_SIG, DHTTYPE);
+DHT dht(CD74HC4067_SIG, DHTTYPE); // plug DHT11 into CD74HC406 analog/digital multiplexer signal pin
 
 // ------------------------------------------------------------------------------------------------------------------------------
 //                                                                                                                          TASKS
