8014ecd031da2f67676d16fe1b3328356183e454
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-02-20 08:17:19 +0000

    Add files via upload

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -389,15 +389,7 @@ struct validationStruct {
 validationStruct validData;
 
 // ------------------------------------------------------------------------------------------------------------------------------
-//                                                                                                                 ANALOGUE WRITE
-
-// value has to be between 0 and valueMax
-void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
-  // calculate duty, 4095 from 2 ^ 12 - 1
-  uint32_t duty = (4095 / valueMax) * min(value, valueMax);
-  // write duty to LEDC
-  ledcWrite(channel, duty);
-}
+//                                                                                                                       RTC TIME
 
 String SerialDisplayRTCDateTime() {
   return String(String(rtc.now().hour()) + ":" + String(rtc.now().minute()) + ":" + String(rtc.now().second()) +
