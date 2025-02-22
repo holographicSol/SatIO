d00f28ab18ce736cae97809cd8cb64e486cc69f5
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-02-22 03:22:00 +0000

    Add files via upload

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -4,76 +4,77 @@
 
                                         SatIO - Written by Benjamin Jack Cullen.
 
-                                                   "The GPS MAster"
+                                                   "The GPS Master"
 
-                              A general purpose programmable satellite and inertial switch.
+                          A general purpose programmable satellite, sensor and inertial platform.
 
-                                      SatIO is the system, a matrix is the program.
-
-            Receives and Processes Transmissions from Satellites and makes the data available for calculations.
-
-            Possible combinations example: 
-
-  10=digit characters   15=lenght of double   3=doubles per function   10=functions per switch   20=switches  190=available functions
-                                                (((10^15 * 3) * 10) * 20) ^ 190
-
-          Currently there are over 200 different checks that can be performed using just several small primitive functions and
-  currently each matrix activation/deactivaion can occur based on up to 10 different checks per 20 switches resulting true or false per switch. 
+                                     SatIO is the system, a matrix is the program.
 
                                     
-                                  Wiring For ESP32 keystudio dev module (with keystudio sheild V1.3)
+                                    Wiring For Keystudio ESP32 PLUS Development Board
 
-                                        Serial Communication Between ESP32 & ATMEGA2560:
-                                        ESP32 io25 (TXD) -> ATMEGA2560 Serial1 (RXD)
-                                        ESP32 null (RXD) -> ATMEGA2560 Serial1 (TXD)
+                                          ESP32: ATMEGA2560:
+                                          ESP32: io25 TXD -> ATMEGA2560: Serial1 RXD
+                                          ESP32: null RXD -> ATMEGA2560: Serial1 TXD
 
-                                        Serial Communication Between ESP32 & WTGPS300P:
-                                        ESP32 io27 (RXD) -> WTGPS300P (TXD) (5v)
+                                          ESP32: WTGPS300P (5v):
+                                          ESP32: io27 RXD -> WTGPS300P: TXD
+                                          ESP32: null TXD -> WTGPS300P: RXD
 
-                                        ESP32 i2C Multiplexing:
-                                        ESP32 i2C          -> TCA9548A i2c Multiplexer: SDA, SCL (3.3v)
-                                        TCA9548A SDA0 SCL0 -> RTC DS3231 SDA, SCL (5v)
+                                          ESP32 i2C: i2C Multiplexing (3.3v):
+                                          ESP32: i2C          -> TCA9548A: SDA, SCL
+                                          TCA9548A: SDA0 SCL0 -> DS3231: SDA, SCL (5v)
 
-                                        ESP32 Analog+Digital Multiplexing:
-                                        ESP32 io4  -> CD74HC4067 Analog/Digital Multiplexer: SIG
-                                        ESP32 io32 -> CD74HC4067 Analog/Digital Multiplexer: S0
-                                        ESP32 io33 -> CD74HC4067 Analog/Digital Multiplexer: S1
-                                        ESP32 io16 -> CD74HC4067 Analog/Digital Multiplexer: S2
-                                        ESP32 io17 -> CD74HC4067 Analog/Digital Multiplexer: S3
-                                        CD74HC4067 C0 -> Photo Resistor SIG
-                                        CD74HC4067 C1 -> DHT11 SIG
+                                          ESP32: Analog/Digital Multiplexing (3.3v):
+                                          ESP32: io4  -> CD74HC4067: SIG
+                                          ESP32: io32 -> CD74HC4067: S0
+                                          ESP32: io33 -> CD74HC4067: S1
+                                          ESP32: io16 -> CD74HC4067: S2
+                                          ESP32: io17 -> CD74HC4067: S3
+                                          CD74HC4067 C0 -> Photo Resistor: SIG
+                                          CD74HC4067 C1 -> DHT11: SIG
 
-                                        ESP32 SDCARD:
-                                        ESP32 io5  -> HW-125 Micro SD Card Module CS (SS)
-                                        ESP32 io23 -> HW-125 Micro SD Card Module DI (MOSI)
-                                        ESP32 io19 -> HW-125 Micro SD Card Module DO (MISO)
-                                        ESP32 io18 -> HW-125 Micro SD Card Module SCK (SCLK)
+                                          ESP32 VSPI: SDCARD (5v):
+                                          ESP32: io5  -> HW-125: CS (SS)
+                                          ESP32: io23 -> HW-125: DI (MOSI)
+                                          ESP32: io19 -> HW-125: DO (MISO)
+                                          ESP32: io18 -> HW-125: SCK (SCLK)
 
-                                                          SENTENCE $SATIO
-                                                                                  
-                          START Tag                Last Sat Time                    Converted Longitude        
-                            |                   |               |                   |               |                  
-                          $SATIO,000000000000.00,000000000000.00,00.00000000000000,00.00000000000000,*Z
-                                |               |               |                 |                              
-                                  DatetimeStamp                  Converted Latitude                                 
+                                          ESP32 HSPI: SSD1351 OLED (5v):
+                                          ESP32: io14 -> SSD1351: SCL/SCLK
+                                          ESP32: io12 -> SSD1351: MISO/DC
+                                          ESP32: io13 -> SSD1351: SDA
+                                          ESP32: io26 -> SSD1351: CS
 
 
-          Ultimately this system is being built as a unit to turn on/off IO/GPIO/relays and or send messages to other controllers,
-                    where potentially anything can be plugged in such as simple modules or pre-programmed MCU's, 
-              making a foundation for other creative projects that may make use of such satellite and or inertial data.
-                  The idea is that each matrix is a compound of logic (limited by memory), and the logic itself
-              is programmable before and after flashing. Allowing for a reusable and general purpose system for any future
-                                              projects requiring use of GPS data.
-  SatIO is also chainable, in that another SatIO can be turned on and run its own logic when a pin turns high/low according to its compound logic.
-                                          Robots and flying machines and automation.
+                                                  SENTENCE $SATIO
+                                                                          
+                  START Tag                Last Sat Time                    Converted Longitude        
+                    |                   |               |                   |               |                  
+                  $SATIO,000000000000.00,000000000000.00,00.00000000000000,00.00000000000000,*Z
+                        |               |               |                 |                              
+                          DatetimeStamp                  Converted Latitude                                 
+
+      Use case: From a clock syncronized with satellites to riding the INS (roll, pitch, yaw) on a fine line to within a certain degree of
+                                    expected drift, if GPS data is stale or unavailable.
+                                        Robots, flying machines and automation.
+
+      Bare bones architecture: SatIO is an extended development platform built on and around ESP32, allowing for many different kinds of projects
+      using SatIO as a standalone system and or integrating SatIO into other systems as a 'part'.
+                                              Extended I2C
+                                              Extended Analogue/Digital.
+                                              Extended VSPI and HSPI.
+                                              Extended IO (using an ATMEGA2560).
+
+      Port Controller: ESP32 receives sensory data, calculates according to programmable matrix, then instructs the port controller to turn pins high/low
+      according to results from the calculations.
+
+      UI: The matrix has been programmable via the UI however the UI has only just been reinstated, after focusing on performance and architecture. The
+      feature of programming the matrix through switches and UI will be reimplemented. Until then, the matrix can be hardcoded for testing purposes.
 
               Requires using modified SiderealPlanets library (hopefully thats okay as the modifications allow calculating rise/set
                   of potentially any celestial body as described in this paper: https://stjarnhimlen.se/comp/riset.html)
 
-                      Use case from a syncronized clock to riding the INS (roll, pitch, yaw) on a fine line to within a certain degree of
-                      expected drift, if GPS data is stale or unavailable.
-
-                        Note: This project is now temporarily headless while architecture & performance are the focus.
   */
 
 // ------------------------------------------------------------------------------------------------------------------------------
