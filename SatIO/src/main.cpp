64e2c54f93eb8fc9f6a60e1b44f1a1b413f02a39
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-03-09 17:22:52 +0000

    Update main.cpp

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -10173,7 +10173,7 @@ void getSensorData(void * pvParameters) {
           // Serial.println("[dht11_hic_0] " + String(sensorData.dht11_hic_0));
         }
 
-        // // sensor 0
+        // sensor 0
         // if (i_chan==0) {
         //   sensorData.sensor_0 = analogRead(CD74HC4067_SIG);
         // }
