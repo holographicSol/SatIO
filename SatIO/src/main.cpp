87d494e92ba7d49af4aebe3924126cc927f68b73
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-03-09 13:54:33 +0000

    Update main.cpp

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -33,13 +33,12 @@
                                           TCA9548A: SDA0 SCL0 -> DS3231: SDA, SCL (5v)
 
                                           ESP32: Analog/Digital Multiplexing (3.3v) (for peripherals):
-                                          ESP32: io4  -> CD74HC4067: SIG
-                                          ESP32: io32 -> CD74HC4067: S0
-                                          ESP32: io33 -> CD74HC4067: S1
-                                          ESP32: io16 -> CD74HC4067: S2
-                                          ESP32: io17 -> CD74HC4067: S3
-                                          CD74HC4067 C0 -> Photo Resistor: SIG
-                                          CD74HC4067 C1 -> DHT11: SIG
+                                          ESP32: io4    -> CD74HC4067: SIG
+                                          ESP32: io32   -> CD74HC4067: S0
+                                          ESP32: io33   -> CD74HC4067: S1
+                                          ESP32: io16   -> CD74HC4067: S2
+                                          ESP32: io17   -> CD74HC4067: S3
+                                          CD74HC4067 C0 -> DHT11: SIG
 
                                           ESP32 VSPI: SDCARD (5v) (for matrix and system data):
                                           ESP32: io5  -> HW-125: CS (SS)
