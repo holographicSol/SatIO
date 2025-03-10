fb69e27c7b68778261022856d6c149648819e938
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-03-10 03:44:47 +0000

    Update main.cpp

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -8529,7 +8529,7 @@ void menuEnter() {
 String getRelatedY(char * data) {
   /*
   returns y value for in ranges checks where x and y are different (not typically required for an in range check where range pertains to x alone).
-  in ranges checks are square (check in square range) where z is square range. example x=lat, y=lon, z=squarerange
+  in ranges checks are square (check in square range) where z is square range. example x=lat, y=lon, z= squarerange = 0.0000100 = approx 1 meter in latitude.
   */
   if (strcmp("DegGNGGARanges", data)==0) {return String(satData.location_longitude_gngga, 10);}
   if (strcmp("DegGNRMCRanges", data)==0) {return String(satData.location_longitude_gnrmc, 10);}
