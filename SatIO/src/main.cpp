7ab496a3e9fac41cb669832b7a44f159a51751ed
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-03-31 10:13:27 +0100

    Update main.cpp

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -3276,7 +3276,7 @@ struct SatDatatruct {
   double millisecondsLat;                                          // used for converting absolute latitude and longitude
   double millisecondsLong;                                         // used for converting absolute latitude and longitude
 
-  // local time converted from rtc utc time (currently for displaying only)
+  // local time converted from rtc utc time (currently only used for display purposes)
   time_t local_time;
   int local_hour = 0;
   int local_minute = 0;
