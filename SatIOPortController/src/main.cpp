dcabb57e9184e0b5e470d40bd7ac47baa9899c6a
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-05-07 04:07:35 +0100

    Update main.cpp

--- SatIOPortController/src/main.cpp
+++ SatIOPortController/src/main.cpp
@@ -25,7 +25,7 @@ ATMEGA2560 SDA 20 -> ESP32 12
 ATMEGA2560 SCL 21 -> ESP32 5
 
 Wiring Matrix Indicators:
-ATMEGA2560 48 -> 20x individually addressable WS2812B's
+ATMEGA2560 48 -> 24x individually addressable WS2812B's
 
 */
 
@@ -42,19 +42,19 @@ ATMEGA2560 48 -> 20x individually addressable WS2812B's
 // ------------------------------------------------------------------------------------------------------------------------------
 //                                                                                                                     INDICATORS
 
-// MATRIX INDICATORS
-#define NUM_LEDS 20
+/*
+Indicators:
+0-19: Matrix Switch State
+20: MATRIX IO ENABLED/DISABLED
+21: DATA
+22: OVERLOAD
+23: SIGNAL
+*/
+
+// INDICATORS
+#define NUM_LEDS 24
 #define DATA_PIN 48
 CRGB leds[NUM_LEDS];
-
-// SIGNAL
-#define LEDSATSIGNALR 51
-#define LEDSATSIGNALG 52
-#define LEDSATSIGNALB 53
-
-// OVERLOAD
-#define LEDOVERLOADR 46
-#define LEDOVERLOADG 47
 
 // ------------------------------------------------------------------------------------------------------------------------------
 
@@ -63,10 +63,16 @@ CRGB leds[NUM_LEDS];
 // ------------------------------------------------------------------------------------------------------------------------------
 //                                                                                                                    MATRIX DATA
 
+bool matrix_io_enabled=false;
+int gps_signal=0;
+bool overload=false;
+bool data_received=false;
 signed int portmap_number;
 bool update_portmap_bool = false;
 
-// reflects matrix switch active/inactive states each loop of matrix switch function
+// ------------------------------------------------------------
+// Matrix switch output high/low
+// ------------------------------------------------------------
 int max_matrix_switch_states = 20;
 bool matrix_switch_state[1][20] = {
   {
@@ -75,7 +81,9 @@ bool matrix_switch_state[1][20] = {
   }
 };
 
-// a placeholder for matrix switch ports
+// ------------------------------------------------------------
+// Matrix switch output ports
+// ------------------------------------------------------------
 signed int matrix_port_map[1][20] = {
   {
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
@@ -83,7 +91,9 @@ signed int matrix_port_map[1][20] = {
   }
 };
 
-// a placeholder for matrix switch ports
+// ------------------------------------------------------------
+// Temporary matrix switch output ports
+// ------------------------------------------------------------
 signed int tmp_matrix_port_map[1][20] = {
   {
     -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
@@ -95,15 +105,14 @@ signed int tmp_matrix_port_map[1][20] = {
 //                                                                                                                    TIME STRUCT
 
 struct TimeStruct {
-  double mainLoopTimeTaken;            // current main loop time
-  unsigned long mainLoopTimeStart;     // time recorded at the start of each iteration of main loop
+  double mainLoopTimeTaken; // current main loop time
+  unsigned long mainLoopTimeStart; // time recorded at the start of each iteration of main loop
 };
 TimeStruct timeData;
 
 // ------------------------------------------------------------------------------------------------------------------------------
 //                                                                                                                      I2C DATA
 
-// Define Slave I2C Address
 #define SLAVE_ADDR 9
 
 struct I2CLinkStruct {
@@ -119,80 +128,81 @@ I2CLinkStruct I2CLink;
 
 void receiveEvent(int) {
 
-  // Serial.println("[slave] read data");
+  // ------------------------------------------------------------
+  // Indicate data received
+  // ------------------------------------------------------------
+  data_received=true;
+  
+  // ------------------------------------------------------------
+  // Read incoming data
+  // ------------------------------------------------------------
   memset(I2CLink.INPUT_BUFFER, 0, sizeof(I2CLink.INPUT_BUFFER));
   Wire.readBytesUntil('\n', I2CLink.INPUT_BUFFER, sizeof(I2CLink.INPUT_BUFFER));
   // Serial.println("[received] " + String(I2CLink.INPUT_BUFFER));
 
-  // get tag token
+  // ------------------------------------------------------------
+  // Tokenize data tag
+  // ------------------------------------------------------------
   I2CLink.token = strtok(I2CLink.INPUT_BUFFER, ",");
 
-  // Port Map
-  if (strcmp(I2CLink.token, "$P")==0) {
+  // ------------------------------------------------------------
+  // Matrix Enabled
+  // ------------------------------------------------------------
+  if (strcmp(I2CLink.token, "$MENABLED")==0) {
     I2CLink.token = strtok(NULL, ",");
-    // Serial.println("[received] portmap: " + String(I2CLink.token));
+    if (strcmp(I2CLink.token, "0") == 0) {matrix_io_enabled=false;}
+    else {matrix_io_enabled=true;}
+  }
+
+  // ------------------------------------------------------------
+  // GPS Signal
+  // ------------------------------------------------------------
+  else if (strcmp(I2CLink.token, "$GPSSIG")==0) {
+    I2CLink.token = strtok(NULL, ",");
+    if      (strcmp(I2CLink.token, "0") == 0) {gps_signal=0;}
+    else if (strcmp(I2CLink.token, "1") == 0) {gps_signal=1;}
+    else if (strcmp(I2CLink.token, "2") == 0) {gps_signal=2;}
+    else                                      {gps_signal=3;}
+  }
+
+  // ------------------------------------------------------------ 
+  // Overload
+  // ------------------------------------------------------------
+  else if (strcmp(I2CLink.token, "$OLOAD")==0) {
+    I2CLink.token = strtok(NULL, ",");
+    if      (strcmp(I2CLink.token, "0") == 0) {overload=false;}
+    else if (strcmp(I2CLink.token, "1") == 0) {overload=true;}
+  }
+
+  // ------------------------------------------------------------
+  // Port Map
+  // ------------------------------------------------------------
+  else if (strcmp(I2CLink.token, "$P")==0) {
+    I2CLink.token = strtok(NULL, ",");
     portmap_number = atoi(I2CLink.token);
     I2CLink.token = strtok(NULL, ",");
-    // Serial.println("[received] portnumber: " + String(I2CLink.token));
     matrix_port_map[0][portmap_number] = atoi(I2CLink.token);
     Serial.println("[portmap] " + String(portmap_number) + String(" [port number] ") + String(matrix_port_map[0][portmap_number]));
   }
 
+  // ------------------------------------------------------------
   // Port States
-  if (strcmp(I2CLink.token, "$M")==0) {
+  // ------------------------------------------------------------
+  else if (strcmp(I2CLink.token, "$M")==0) {
     I2CLink.token = strtok(NULL, ",");
-    // Serial.println("[received] portmap: " + String(I2CLink.token));
     portmap_number = atoi(I2CLink.token);
     I2CLink.token = strtok(NULL, ",");
-    // Serial.println("[received] portnumber: " + String(I2CLink.token));
     matrix_switch_state[0][portmap_number] = atoi(I2CLink.token);
     Serial.println("[portmap] " + String(portmap_number) + String(" [port state] ") + String(matrix_switch_state[0][portmap_number]));
-  }
-
-  // Satellite Count and HDOP Precision Factor Indicator
-  if (strcmp(I2CLink.token, "$GPSSIG")==0) {
-    I2CLink.token = strtok(NULL, ",");
-    // Serial.println("[received] Satellite Count and HDOP Precision Factor: " + String(I2CLink.token));
-    if (strcmp(I2CLink.token, "0") == 0)
-      // red
-      {
-        digitalWrite(LEDSATSIGNALG, LOW);
-        digitalWrite(LEDSATSIGNALB, LOW);
-        digitalWrite(LEDSATSIGNALR, LOW);
-        digitalWrite(LEDSATSIGNALR, HIGH);
-      }
-      // green
-      else if (strcmp(I2CLink.token, "1") == 0)
-      {
-        digitalWrite(LEDSATSIGNALR, LOW);
-        digitalWrite(LEDSATSIGNALB, LOW);
-        digitalWrite(LEDSATSIGNALG, LOW);   
-        digitalWrite(LEDSATSIGNALG, HIGH);
-      }
-      // blue
-      else if (strcmp(I2CLink.token, "2") == 0)
-      {
-        digitalWrite(LEDSATSIGNALR, LOW);
-        digitalWrite(LEDSATSIGNALG, LOW);
-        digitalWrite(LEDSATSIGNALB, LOW);
-        digitalWrite(LEDSATSIGNALB, HIGH);
-      }
-  }
-
-  // Overload Indicator
-  if (strcmp(I2CLink.token, "$OLOAD")==0) {
-    I2CLink.token = strtok(NULL, ",");
-    // Serial.println("[received] Overload Indicator: " + String(I2CLink.token));
-    if (strcmp(I2CLink.token, "0") == 0) {digitalWrite(LEDOVERLOADR, LOW); digitalWrite(LEDOVERLOADG, LOW);}
-    else if (strcmp(I2CLink.token, "1") == 0) {digitalWrite(LEDOVERLOADR, HIGH); digitalWrite(LEDOVERLOADG, HIGH);}
   }
 }
 
 void requestEvent() {
+  // ------------------------------------------------------------
   // Write Bytes of Chars
-  // Serial.println("[slave] write data");
+  // ------------------------------------------------------------
   memset(I2CLink.OUTPUT_BUFFER, 0, sizeof(I2CLink.OUTPUT_BUFFER));
-  for (int i=0;i<sizeof(I2CLink.OUTPUT_BUFFER);i++) {I2CLink.OUTPUT_BUFFER[i] = (byte)I2CLink.TMP_BUFFER[i];}
+  for (byte i=0;i<sizeof(I2CLink.OUTPUT_BUFFER);i++) {I2CLink.OUTPUT_BUFFER[i] = (byte)I2CLink.TMP_BUFFER[i];}
   Wire.write(I2CLink.OUTPUT_BUFFER, sizeof(I2CLink.OUTPUT_BUFFER));
 }
 
@@ -200,35 +210,71 @@ void requestEvent() {
 //                                                                                                    PORT CONTROLLER
 
 void satIOPortController() {
-  // Serial.println("[satIOPortController] ");
-
-  // write switch state to pins specified in port map
+  // ------------------------------------------------------------
+  // Write switch state to pins specified in port map
+  // ------------------------------------------------------------
   for (int i=0; i<20; i++) {
     if (update_portmap_bool==true) {
       if (matrix_port_map[0][i] != tmp_matrix_port_map[0][i]) {
 
+        // ------------------------------------------------------------
         // set a depricated port mapping to default values
+        // ------------------------------------------------------------
         digitalWrite(matrix_port_map[0][i], LOW);
         pinMode(matrix_port_map[0][i], INPUT);
 
         Serial.println("[portmap] updating port: " + String(matrix_port_map[0][i]) + " -> " + String(tmp_matrix_port_map[0][i]));
 
+        // ------------------------------------------------------------
         // setup new port
+        // ------------------------------------------------------------
         matrix_port_map[0][i]=tmp_matrix_port_map[0][i];
         pinMode(matrix_port_map[0][i], OUTPUT);
       }
     }
 
+    // ------------------------------------------------------------
     // set port high/low
+    // ------------------------------------------------------------
     digitalWrite(matrix_port_map[0][i], matrix_switch_state[0][i]);
 
+    // ------------------------------------------------------------
     // set matrix indicator
-    if (matrix_switch_state[0][i]==1) {leds[i] = CRGB::Yellow; FastLED.show();}
+    // ------------------------------------------------------------
+    if (matrix_switch_state[0][i]==1) {leds[i] = CRGB::Red; FastLED.show();}
     else {leds[i] = CRGB::Black; FastLED.show();}
 
+    // ------------------------------------------------------------
     // uncomment to debug
+    // ------------------------------------------------------------
     // Serial.println("[" + String(matrix_port_map[0][i]) + "] " + String(digitalRead(matrix_port_map[0][i])));
   }
+
+  // ------------------------------------------------------------
+  // Indicate matrix enabled (matrix data is being received)
+  // ------------------------------------------------------------
+  if (matrix_io_enabled==true) {leds[20] = CRGB::Blue; FastLED.show();}
+  else {leds[20] = CRGB::Red; FastLED.show();}
+
+  // ------------------------------------------------------------
+  // Indicate data received
+  // ------------------------------------------------------------
+  if (data_received==true) {leds[21] = CRGB::Red; FastLED.show();}
+  else {leds[21] = CRGB::Black; FastLED.show();}
+
+  // ------------------------------------------------------------
+  // Indicate overload
+  // ------------------------------------------------------------
+  if (overload==true) {leds[22] = CRGB::Yellow; FastLED.show();}
+  else {leds[22] = CRGB::Black; FastLED.show();}
+
+  // ------------------------------------------------------------
+  // Indicate GPS signal
+  // ------------------------------------------------------------
+  if      (gps_signal==0) {leds[23] = CRGB::Red; FastLED.show();}
+  else if (gps_signal==1) {leds[23] = CRGB::Green; FastLED.show();}
+  else if (gps_signal==2) {leds[23] = CRGB::Blue; FastLED.show();}
+  else                    {leds[23] = CRGB::Red; FastLED.show();}
 }
 
 // ------------------------------------------------------------------------------------------------------------------
@@ -236,40 +282,38 @@ void satIOPortController() {
 
 void setup() {
 
+  // ------------------------------------------------------------
   // Serial
+  // ------------------------------------------------------------
   Serial.setTimeout(50); // ensure this is set before begin()
   Serial.begin(115200);  while(!Serial);
 
+  // ------------------------------------------------------------
   // Matrix indicators
+  // ------------------------------------------------------------
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 
+  // ------------------------------------------------------------
   // Matrix switches
+  // ------------------------------------------------------------
   for (int i=0; i<20; i++) {
     pinMode(matrix_port_map[0][i], OUTPUT);
     digitalWrite(matrix_port_map[0][i], LOW);
   }
 
-  // Satellite signal indicator
-  pinMode(LEDSATSIGNALR, OUTPUT);
-  pinMode(LEDSATSIGNALG, OUTPUT);
-  pinMode(LEDSATSIGNALB, OUTPUT);
-  digitalWrite(LEDSATSIGNALR, LOW);
-  digitalWrite(LEDSATSIGNALG, LOW);
-  digitalWrite(LEDSATSIGNALB, LOW);
-
-  // Overload indicator
-  pinMode(LEDOVERLOADR, OUTPUT);
-  pinMode(LEDOVERLOADG, OUTPUT);
-  digitalWrite(LEDOVERLOADR, LOW);
-  digitalWrite(LEDOVERLOADG, LOW);
-
+  // ------------------------------------------------------------
   // Initialize I2C communications as Slave
+  // ------------------------------------------------------------
   Wire.begin(SLAVE_ADDR);
 
+  // ------------------------------------------------------------
   // Function to run when data requested from master
+  // ------------------------------------------------------------
   Wire.onRequest(requestEvent);
 
+  // ------------------------------------------------------------
   // Function to run when data received from master
+  // ------------------------------------------------------------
   Wire.onReceive(receiveEvent);
 
   Serial.println("starting...");
@@ -279,16 +323,28 @@ void setup() {
 //                                                                                                         MAIN LOOP
 
 void loop() {
-
+  // ------------------------------------------------------------
+  // uncomment to debug
+  // ------------------------------------------------------------
   // Serial.println("---------------------------------------");
-  
   // Serial.println("[loop] ");
-
   // timeData.mainLoopTimeStart = millis();  // store current time to measure this loop time
-  
-  satIOPortController();
 
+  // ------------------------------------------------------------
+  // reset data indicator
+  // ------------------------------------------------------------
+  data_received=false;
+  
+  // ------------------------------------------------------------
+  // run port controller function
+  // ------------------------------------------------------------
+  satIOPortController();
+  
   delay(1);
+
+  // ------------------------------------------------------------
+  // uncomment to debug
+  // ------------------------------------------------------------
   // timeData.mainLoopTimeTaken = millis() - timeData.mainLoopTimeStart;  // store time taken to complete
   // Serial.print("[looptime] "); Serial.println(timeData.mainLoopTimeTaken);
 }
