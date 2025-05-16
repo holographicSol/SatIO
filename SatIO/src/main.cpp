d75a4a031e455f3092dcdd65985058244137fe25
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-05-16 22:37:36 +0100

    Update main.cpp

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -19054,6 +19054,33 @@ static void CmdProcess(void) {
         else {Serial.println("[command failed]");}
       }
       // ------------------------------------------------------------------------------------------------------------------------------
+      //                                                                                                         SET MATRIX SWITCH PORT
+      // ------------------------------------------------------------------------------------------------------------------------------
+      else if (strncmp(CMD_BUFFER, "switch matrix port \r", 19)==0) {
+        TMP_CMD_TOKEN=strtok(CMD_BUFFER, " ");
+        ITER_TMP_CMD_TOKEN=0;
+        COMMAND_PASS=0;
+        while (TMP_CMD_TOKEN!=NULL) {
+          if      (ITER_TMP_CMD_TOKEN==3) {if (is_all_digits(TMP_CMD_TOKEN)) {TMP_CMD_STRING_0=TMP_CMD_TOKEN; COMMAND_PASS++;}}
+          else if (ITER_TMP_CMD_TOKEN==4) {TMP_CMD_STRING_1=TMP_CMD_TOKEN; COMMAND_PASS++;} // ToDo: sanitize last token
+          TMP_CMD_TOKEN=strtok(NULL, " ");
+          ITER_TMP_CMD_TOKEN++;
+        }
+        // --------------------------------------------------------------------
+        // uncomment to debug
+        // --------------------------------------------------------------------
+        Serial.println("[TMP_CMD_STRING_0] " + String(TMP_CMD_STRING_0));
+        Serial.println("[TMP_CMD_STRING_1] " + String(TMP_CMD_STRING_1));
+        Serial.println("[COMMAND_PASS] " + String(COMMAND_PASS));
+        if (COMMAND_PASS==2) {
+          // --------------------------------------------------------------------
+          // set switch port
+          // --------------------------------------------------------------------
+          matrixData.matrix_port_map[0][atoi(TMP_CMD_STRING_0.c_str())]=atoi(TMP_CMD_STRING_1.c_str());
+        }
+        else {Serial.println("[command failed]");}
+      }
+      // ------------------------------------------------------------------------------------------------------------------------------
       //                                                                                                              SET MATRIX SWITCH
       // ------------------------------------------------------------------------------------------------------------------------------
       /*
