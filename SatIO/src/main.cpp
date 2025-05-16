59c85c8e12339f93d5d59b67a34b25b81fc47d60
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-05-17 00:32:14 +0100

    Update main.cpp

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -18870,6 +18870,7 @@ static void CmdProcess(void) {
     //                                                             SPECIFIC REQUEST SERIAL OUTPUT: AVAILABLE MATRIX FUNCTIONS VERBOSE
     // ------------------------------------------------------------------------------------------------------------------------------
     else if (strcmp(CMD_BUFFER, "print matrix available functions -v\r")==0) {
+      Serial.println("[Available Matrix Functions] ");
       for (int i=0; i<matrixData.max_matrix_function_names; i++) {
         Serial.println("[" + String(i) + "] " + String(matrixData.matrix_function_names[i]));
       }
