2eeea731e7e97e35ccfb37dbed635a3d5e9e73fc
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-07-11 02:02:51 +0100

    Update main.cpp

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -14646,7 +14646,8 @@ String getRelatedX(char * data) {
     else if (strcmp("NeptuneRise", data)==0) {return String(siderealPlanetData.neptune_r);}
     else if (strcmp("NeptuneSet", data)==0) {return String(siderealPlanetData.neptune_s);}
   }
-  else if (strncmp(data, "DHT11H0", strlen("DHT11H0"))==0) {
+  else if (strncmp(data, "DHT11", strlen("DHT11"))==0) {
+    Serial.println(data);
     if (strcmp("DHT11H0Under", data)==0) {return String(sensorData.dht11_h_0);}
     else if (strcmp("DHT11H0Over", data)==0) {return String(sensorData.dht11_h_0);}
     else if (strcmp("DHT11H0Equal", data)==0) {return String(sensorData.dht11_h_0);}
