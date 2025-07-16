6dbe8a389bc8c76c8a96ebaf21471e72c9eb5141
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-07-16 12:41:58 +0100

    Update main.cpp

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -15706,8 +15706,8 @@ void drawZodiac() {
   tft.drawLine(sundial[0][0], sundial[0][1], sundial[0][2], sundial[0][3], RGB_COLOR16(0,0,0)); // clear old
   sundial[0][0]=earth_ui_x+1;
   sundial[0][1]=earth_ui_y+1;
-  sundial[0][2]=earth_ui_x + (int)(cos(eclipticLong * PI / 180.0) * 3);
-  sundial[0][3]=earth_ui_y + (int)(sin(eclipticLong * PI / 180.0) * 3);
+  sundial[0][2]=earth_ui_x + (int)(cos(eclipticLong * PI / 180.0) * 4);
+  sundial[0][3]=earth_ui_y + (int)(sin(eclipticLong * PI / 180.0) * 4);
   tft.drawLine(sundial[0][0], sundial[0][1], sundial[0][2], sundial[0][3], TFT_YELLOW); // draw new
 }
 
