3cfc9c52ab37c80da7638a18592a175d9ea13ac6
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-07-20 16:53:04 +0100

    Update main.cpp

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -13,11 +13,11 @@
   track the tides, if the system is aware moon/planetary positioning and datetime then marine life values may also be inferred relative
   to the inferred tide values and known datetime. There is a lot of data that can be used in many ways, with a kind of network effect.
   Or more simply 'its one hell of a switch'.
-  
+
   The amount of data and I/O available on this project makes this project a more than adequately general purpose platform for all
   kinds of future projects, from universally aware LLMs harnessing SatIO's optional serial output, to sensor drones, or even just a
   GPS syncronized astro clock. Everything other than the ESP32 running SatIO should be considered as optional and modular, meaning
-  SatIO builds can be from a headless chip with GPS pouring out data over serial, or a full system with I/O and display.
+  SatIO builds can be from a headless chip with GPS pouring out data over serial, or a full system with extra I/O and display.
 
   Short of quantum navigation on a microchip, GPS is currently used for navigation, providing values that many more values
   can be calculated from, providing there is not something potentially terminally wrong with the universe.
