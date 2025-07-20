cf0c122cee72ed5db54daf6e94eafccd636f1b6b
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-07-20 16:36:38 +0100

    Update main.cpp

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -10,7 +10,8 @@
   Applications? Potentially infinite applications.
 
   Inference in Bayesian Reasoning? Moon tracking for example can be used to track the moon, it can also be used for one example; to 
-  track the tides. There is a lot of data that can be used in many ways, with a kind of network effect.
+  track the tides, if the system is aware of certain tide values and datetime then marine life values may also be inferred relative
+  to the inferred tide values and known datetime. There is a lot of data that can be used in many ways, with a kind of network effect.
   Or more simply 'its one hell of a switch'.
   The amount of data and I/O available on this project makes this project an extremely general purpose platform for all kinds of future projects.
 
