6f28236e771261c1b95ed1a486fdd891e3ba2cfa
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2025-08-06 15:56:39 +0100

    Update main.cpp

--- SatIO/src/main.cpp
+++ SatIO/src/main.cpp
@@ -27,7 +27,8 @@
   Matrix logic is an attempt to maximize programmable potential and hardware configuration is designed to attempt maximum IO potential.
   If more output is needed then add another I2C port controller, if more input is needed then add another custom I2C sensor module.
   If more matrix logic is needed then build on another MCU, matrix switches and switch functions have been balanced to allow for plenty
-  of switches and functions for said switches, with regards to available memory/storage.
+  of switches and functions for said switches, with regards to available memory/storage (10 functions per switch not including switch linking
+  where logic can be stacked accross multiple/all switches/memory allocations).
 
         Design: Break out all the things and build I2C peripherals as required to orbit the ESP32/Central-MCU.
 
